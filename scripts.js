cb8a4275eec5d0e07e80314f076d08d2f7efcf73
hassansanto <hasibulhassansanto3@gmail.com>
2024-11-03 18:02:56 +0600

    initialization

---
+++ scripts.js
@@ -0,0 +1,7 @@
+describe("Swag Lab automation" , =>{
+    it{"Successfully product purchase journey", async()=>{
+        await browser.url("https://www.saucedemo.com/");
+        await $("//input[@placeholder ="Username"]").click();
+        await $
+    }}
+})
